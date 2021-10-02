34504248f17fbfb75d0ff97bee7363e830d1007e
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-02 02:26:42 -0500

    Update MainCode.js

--- MainCode.js
+++ MainCode.js
@@ -8,30 +8,32 @@ toolBtn.style.left='1%';
 toolBtn.style.top='3%';
 document.body.appendChild(toolBtn);
 toolBtn.onmouseover = function()
-          {this.style.backgroundColor = 'LightCyan';};
+          {this.style.backgroundColor = 'Lavender';};
 toolBtn.onmouseout = function()
           {this.style.backgroundColor = 'Azure';};
+var showClick = false;
 toolBtn.onclick = function () {
-
+if (showClick = false){
+showClick = true;
 var inelBtn = document.createElement("button");
 var autoclBtn= document.createElement("button");
 var xrayBtn = document.createElement("button");
 var wrtxtBtn = document.createElement("button");
 var txtedBtn = document.createElement("button");
-inelBtn.innerHTML=autoclBtn.innerHTML=xrayBtn.innerHTML=wrtxtBtn.innerHTML=txtedBtn.innerHTML = "food";
-[inelBtn.style.top,autoclBtn.style.top,xrayBtn.style.top,wrtxtBtn.style.top,txtedBtn.style.top] = ['10%','15%','20%','25%','30%'];
+[inelBtn.innerHTML,autoclBtn.innerHTML,xrayBtn.innerHTML,wrtxtBtn.innerHTML,txtedBtn.innerHTML] = ["Inspect Element","Autoclicker","Webpage X-Ray","Text Writer","Text Editor",];
+[inelBtn.style.top,autoclBtn.style.top,xrayBtn.style.top,wrtxtBtn.style.top,txtedBtn.style.top] = ['10%','16%','22%','28%','34%'];
 inelBtn.style.position=autoclBtn.style.position=xrayBtn.style.position=wrtxtBtn.style.position=txtedBtn.style.position = 'fixed';
 inelBtn.style.backgroundColor=autoclBtn.style.backgroundColor=xrayBtn.style.backgroundColor=wrtxtBtn.style.backgroundColor=txtedBtn.style.backgroundColor = 'Azure';
-inelBtn.style.height=autoclBtn.style.height=xrayBtn.style.height=wrtxtBtn.style.height=txtedBtn.style.height = "37px";
-inelBtn.style.width=autoclBtn.style.width=xrayBtn.style.width=wrtxtBtn.style.width=txtedBtn.style.width = "75px";
-inelBtn.style.left=autoclBtn.style.left=xrayBtn.style.left=wrtxtBtn.style.left=txtedBtn.style.left = '1%';
+inelBtn.style.height=autoclBtn.style.height=xrayBtn.style.height=wrtxtBtn.style.height=txtedBtn.style.height = "38px";
+inelBtn.style.width=autoclBtn.style.width=xrayBtn.style.width=wrtxtBtn.style.width=txtedBtn.style.width = "90px";
+inelBtn.style.left=autoclBtn.style.left=xrayBtn.style.left=wrtxtBtn.style.left=txtedBtn.style.left = '1.25%';
 document.body.appendChild(inelBtn);document.body.appendChild(autoclBtn);document.body.appendChild(xrayBtn);document.body.appendChild(wrtxtBtn);document.body.appendChild(txtedBtn);
-          
-          
-Btn.onmouseover = function()
-          {this.style.backgroundColor = 'LightCyan';};
-Btn.onmouseout = function()
-          {this.style.backgroundColor = 'Azure';};
-
-};
-  
+inelBtn.style.visiblility=autoclBtn.style.visibility=xrayBtn.style.visibility=wrtxtBtn.style.visibility=txtedBtn.style.visibility = 'visible';
+inelBtn.onmouseover=autoclBtn.onmouseover=xrayBtn.onmouseover=wrtxtBtn.onmouseover=txtedBtn.onmouseover = function()
+{this.style.backgroundColor = 'LightCyan';};
+inelBtn.onmouseout=autoclBtn.onmouseout=xrayBtn.onmouseout=wrtxtBtn.onmouseout=txtedBtn.onmouseout = function()
+{this.style.backgroundColor = 'Azure';};
+}
+} else{  
+showClick = false;
+inelBtn.style.visiblility=autoclBtn.style.visibility=xrayBtn.style.visibility=wrtxtBtn.style.visibility=txtedBtn.style.visibility = 'hidden';};
