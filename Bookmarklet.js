56056655463e3bcb59c5be240f5685fa3154a2bb
K0d3c4t <89653781+K0d3c4t@users.noreply.github.com>
2021-10-04 08:03:29 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/K0d3c4t/Master-Code-Project/main/MainCode.js") .then((res) => res.text() .then((t) => eval(t)))
+javascript:fetch("https://raw.githubusercontent.com/K0d3c4t/Master-Webpage-Editor-Bookmarklet/main/MainCode.js") .then((res) => res.text() .then((t) => eval(t)))
